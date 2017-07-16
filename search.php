1446c61d4613d321e04bdb8231dc62a0aaa5fc4e
zelalem <mzench@gmail.com>
2017-07-16 11:32:24 -0400

    Master

---
+++ search.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Search</title>
+  <meta charset="utf-8">
+  <meta name = "format-detection" content = "telephone=no" />
+  <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="css/grid.css">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="js/jquery.js"></script>
+  <script src="js/jquery-migrate-1.2.1.js"></script>
+  <script src="search/search.js"></script>
+
+
+
+  <!--[if lt IE 8]>
+  <div style=' clear: both; text-align:center; position: relative;'>
+    <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+    <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+  </a>
+  </div>
+  <![endif]-->
+  <!--[if lt IE 9]>
+  <script src="js/html5shiv.js"></script>
+  <link rel="stylesheet" type="text/css" media="screen" href="css/ie.css">
+  <![endif]-->
+  
+</head>
+
+<body>
+  <!--========================================================
+                            HEADER 
+  =========================================================-->
+  <header id="header" class="header_padding">
+    <div class="container">
+      <div class="row">
+        <div class="grid_12">
+          <nav>
+            <ul class="sf-menu">
+            <li class="current"><a href="index.html">Home</a></li>
+            <li><a href="practicearea.html">Practice area</a></li>
+            <li><a href="team.html">Team</a></li>
+
+            <li><a href="testimonials.html">Testimonials</a></li>
+            <li><a href="contacts.html">Contacts</a></li>
+            <li><a href="resources.html">Resources</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="grid_6">
+          <div class="logo">
+            <h1><a href="index.html"><img src="images/logo.png" alt="logo"></a> </h1>
+          </div>
+        </div>
+        <div class="grid_6">
+          <div class="shop-phone">
+            <i class="fa fa-phone-square"></i>
+            <span> 
+              GIVE ME A CALL:
+              <strong>+1 800 567 45 56</strong>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!--========================================================
+                            CONTENT 
+  =========================================================-->
+
+  <section id="content">
+    <div class="bg-inset-6">
+      <div class="container">
+        <div class="row">
+          <div class="grid_12">
+            <h3 class="search-results_title text-color-dark">Search result:</h3>
+            <div id="search-results"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<!--========================================================
+                            FOOTER 
+=========================================================-->
+  <footer id="footer">
+    <div class="container">
+      <div class="row">
+        <div class="grid_6">
+          <span id="copyright-title">The Law Office of Alan Gudeta</span>
+          <span>© 2017 |
+            <a href="#">Privacy Policy</a>
+          </span>
+        </div>
+        <div class="grid_6">
+          <div class="social-links">
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-rss"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-google-plus"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+<script src="js/script.js"></script>
+</body>
+</html>
